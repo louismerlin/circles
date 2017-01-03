8aa3e3a652a4262dff7007c2b8b7ece6fb11202b
Louis Merlin <louis.merlin@epfl.ch>
2017-01-03 09:42:35 +0100

    Create basic project from ripples

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/louismerlin/circles/game"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	// websocket server
+	server := backend.NewServer("/entry")
+	go server.Listen()
+
+	// static files
+	http.Handle("/", http.FileServer(http.Dir("webroot")))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
